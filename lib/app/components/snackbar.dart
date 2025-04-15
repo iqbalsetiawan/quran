54a183070b7db138af0f0585d5c2917e366e40aa
Iqbal Setiawan <ibal.setiawan08@gmail.com>
2025-04-15 14:56:03 +0700

    Remove TODO

--- lib/app/components/snackbar.dart
+++ lib/app/components/snackbar.dart
@@ -2,7 +2,6 @@ import 'package:get/get.dart';
 import 'package:quran/app/constants/color.dart';
 
 class Snackbar {
-  // TODO: Localization
   static void showSnackbar(String? title, String? message) {
     Get.snackbar(
       title ?? 'Error',
